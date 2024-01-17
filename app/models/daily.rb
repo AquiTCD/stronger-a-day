cc559b78f98fb578de284c09725d4f9b4ae292cd
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2024-01-17 22:46:16 +0900

    models

---
+++ app/models/daily.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: dailies
+#
+#  id           :bigint           not null, primary key
+#  round        :integer          default(1), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  character_id :bigint           not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_dailies_on_character_id  (character_id)
+#  index_dailies_on_user_id       (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (character_id => characters.id)
+#  fk_rails_...  (user_id => users.id)
+#
+class Daily < ApplicationRecord
+  belongs_to :user
+  belongs_to :character
+  has_many :daily_challenges, dependent: :destroy
+  has_many :challenges, through: :daily_challenges
+  has_many :daily_results, dependent: :destroy
+end
---
+++ app/models/daily_challenge.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: daily_challenges
+#
+#  id            :bigint           not null, primary key
+#  failure_count :integer          default(0), not null
+#  success_count :integer          default(0), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  challenge_id  :bigint           not null
+#  daily_id      :bigint           not null
+#
+# Indexes
+#
+#  index_daily_challenges_on_challenge_id  (challenge_id)
+#  index_daily_challenges_on_daily_id      (daily_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (challenge_id => challenges.id)
+#  fk_rails_...  (daily_id => dailies.id)
+#
+class DailyChallenge < ApplicationRecord
+  belongs_to :daily
+  belongs_to :challenge
+end
