190d9efd4bbac1f15138e8cfb9c348cf18362785
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2024-02-03 09:45:14 +0900

    disable reset dB

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,7 +6,7 @@ bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
 
-./bin/rake RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 db:reset
+# ./bin/rake RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 db:reset
 
 ./bin/rails db:migrate
 ./bin/rails db:seed_fu
