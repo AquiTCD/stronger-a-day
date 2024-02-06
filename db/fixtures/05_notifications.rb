84a8088a019f8380589f2149b3463d4a9959f796
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2024-02-06 21:06:00 +0900

    fixture

--- db/fixtures/05_notifications.rb
+++ db/fixtures/05_notifications.rb
@@ -9,6 +9,40 @@ Notifications = [
     released_at: nil,
   },
   {
+    subject: "アップデート: v0.1.1",
+    content:<<~TXT,
+        - マイページ機能を追加
+        - 他ユーザーのチャレンジ表示機能を追加
+      TXT
+    importance: :normal,
+    released_at: "2024-02-04 18:45:00".in_time_zone,
+  },
+  {
+    subject: "アップデート: v0.1.2",
+    content:<<~TXT,
+        - チャレンジ登録モーダルを追加
+      TXT
+    importance: :normal,
+    released_at: "2024-02-04 22:30:00".in_time_zone,
+  },
+  {
+    subject: "アップデート: v0.1.3",
+    content:<<~TXT,
+        - チャレンジ登録モーダルを追加
+      TXT
+    importance: :normal,
+    released_at: "2024-02-05 10:00:00".in_time_zone,
+  },
+  {
+    subject: "クローズドベータのお知らせ",
+    content:<<~TXT,
+        「ヒトツヨ」は現在ベータ版です
+        お気づきの点や感想などありましたらぜひ [フィードバック](https://docs.google.com/forms/d/1ab8dY1QGskNXxnQG1Z-Kwsga313ndkinUot3D9uijRg/edit) にご協力ください
+      TXT
+    importance: :higher,
+    released_at: nil,
+  },
+  {
     subject: "プレイ履歴リセットについて",
     content:<<~TXT,
         今後の機能拡充に向けて内部的なデータ構成を変更しました
@@ -21,9 +55,9 @@ Notifications = [
     released_at: "2024-02-06 10:00:00".in_time_zone,
   },
   {
-    subject: "アップデート: v0.1.3",
+    subject: "アップデート: v0.1.4",
     content:<<~TXT,
-        お知らせ機能を追加しました
+        - お知らせ機能を追加
       TXT
     importance: :normal,
   }
