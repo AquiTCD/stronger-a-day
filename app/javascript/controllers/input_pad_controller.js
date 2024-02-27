1c123e533110d7835d33319dff4675b15a946b95
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2024-02-27 09:48:11 +0900

    save caret position

--- app/javascript/controllers/input_pad_controller.js
+++ app/javascript/controllers/input_pad_controller.js
@@ -9,13 +9,35 @@ export default class extends Controller {
   }
 
   add(event) {
-    this.inputTarget.value = this.inputTarget.value + event.params.value
+    const inputValue = isNaN(event.params.value) ? event.params.value :  String(event.params.value)
+    const caretPosition = this.textarea.selectionStart
+    const beforeCaret = this.textarea.value.substr(0, caretPosition)
+    const afterCaret = this.textarea.value.substr(caretPosition)
+
+    this.textarea.value = `${beforeCaret}${inputValue}${afterCaret}`
+    const newCaretPosition = caretPosition + inputValue.length
     this.input()
+
+    this.textarea.setSelectionRange(newCaretPosition, newCaretPosition);
+    this.textarea.focus()
   }
 
   delete() {
-    this.inputTarget.value = this.inputTarget.value.slice(0, -1)
+    const caretPosition = this.textarea.selectionStart
+    if (caretPosition === 0) {
+      this.textarea.focus()
+      return
+    }
+
+    const beforeCaret = this.textarea.value.substr(0, caretPosition)
+    const afterCaret = this.textarea.value.substr(caretPosition)
+
+    this.textarea.value = `${beforeCaret.slice(0, -1)}${afterCaret}`
+    const newCaretPosition = caretPosition - 1
     this.input()
+
+    this.textarea.setSelectionRange(newCaretPosition, newCaretPosition);
+    this.textarea.focus()
   }
 
   input() {
